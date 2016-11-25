558329f2d6cbb87b299098794cdeb591c7778a92
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-11-25 11:37:48 +0100

    modified to accept some providers and ct

--- app/src/app.js
+++ app/src/app.js
@@ -11,6 +11,7 @@ var config = require('config');
 var koaLogger = require('koa-logger');
 var loader = require('loader');
 var ErrorSerializer = require('serializers/errorSerializer');
+const ctRegisterMicroservice = require('ct-register-microservice-node');
 
 // instance of koa
 var app = koa();
@@ -46,18 +47,23 @@ var server = require('http').Server(app.callback());
 var port = process.env.PORT || config.get('service.port');
 //
 server.listen(port, function() {
-    var p = require('vizz.microservice-client').register({
-        id: config.get('service.id'),
-        name: config.get('service.name'),
-        dirConfig: path.join(__dirname, '../microservice'),
-        dirPackage: path.join(__dirname, '../../'),
-        logger: logger,
-        app: app
-    });
-    p.then(function() {}, function(err) {
-        logger.error(err);
-        process.exit(1);
-    });
+
+    ctRegisterMicroservice.register({
+            info: require('../microservice/register.json'),
+            swagger: require('../microservice/public-swagger.json'),
+            mode: process.env.NODE_ENV === 'dev' ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
+            framework: ctRegisterMicroservice.KOA1,
+            app,
+            logger,
+            name: config.get('service.name'),
+            ctUrl: process.env.CT_URL,
+            url: process.env.LOCAL_URL,
+            active: true,
+        }).then(() => {}, (err) => {
+            logger.error(err);
+            process.exit(1);
+        });
+
 });
 
 logger.info('Server started in port:' + port);
